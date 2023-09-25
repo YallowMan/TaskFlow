8825eb8e6e08ebc9f78e6330636dd0bd6c98e5fc
Carlos Garcia <79238305+YallowMan@users.noreply.github.com>
2023-09-24 19:33:05 -0700

    Add files via upload

--- design-blocks/custom_blocks.js
+++ design-blocks/custom_blocks.js
@@ -1,16 +1,32 @@
-Blockly.Blocks['custom_block'] = {
+Blockly.Blocks['create_task'] = {
     init: function() {
-        this.appendDummyInput()
-            .appendField("custom block");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(230);
-        this.setTooltip("");
-        this.setHelpUrl("");
+      this.appendValueInput("NAME")
+          .setCheck("String");
+      this.setColour(230);
+   this.setTooltip("");
+   this.setHelpUrl("");
     }
+  };
+
+
+
+Blockly.JavaScript['create_task'] = function(block) {
+    // Custom block JavaScript code.
+    var code = '// custom block code\n';
+    return code;
 };
 
-Blockly.JavaScript['custom_block'] = function(block) {
+Blockly.Blocks['assign_task'] = {
+    init: function() {
+      this.appendValueInput("NAME")
+          .setCheck("String");
+      this.setColour(90);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+  Blockly.JavaScript['assign_task'] = function(block) {
     // Custom block JavaScript code.
     var code = '// custom block code\n';
     return code;

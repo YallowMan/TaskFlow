fa0005c49dd41e354e105ecfd9247cd9ec3588de
Carlos Garcia <79238305+YallowMan@users.noreply.github.com>
2023-10-01 19:47:43 -0700

    Add files via upload

--- test/custom_blocks.js
+++ test/custom_blocks.js
@@ -17,6 +17,24 @@ Blockly.Blocks['create_task'] = {
   }
 };
 
+// Define the "task_details" block
+Blockly.Blocks['task_details'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("Task Details");
+      this.appendValueInput("task_name")
+          .setCheck("String")
+          .appendField(new Blockly.FieldTextInput("default"), "Task Name");
+      this.appendValueInput("task_description")
+          .setCheck("String")
+          .appendField(new Blockly.FieldTextInput("default"), "Task Description");
+      this.setOutput(true, "Array");
+      this.setColour(230);
+      this.setTooltip("Provide the task name and description");
+      this.setHelpUrl("");
+    }
+  };
+
 // Define the JavaScript code to be generated for the "create_task" block
 Blockly.JavaScript['create_task'] = function(block) {
   var task_name = Blockly.JavaScript.valueToCode(block, 'task_name', Blockly.JavaScript.ORDER_ATOMIC);

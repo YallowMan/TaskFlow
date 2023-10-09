41dfc9741c03e8744eee2aed0d69c2a56e0bdd21
Carlos Garcia <79238305+YallowMan@users.noreply.github.com>
2023-10-08 17:35:10 -0700

    Update custom_blocks.js with main code
    Added changes added by emily to main for semantics and functionality of some of the main code blocks like creating, assigning, set dependency and updating a task

--- design-blocks/custom_blocks.js
+++ design-blocks/custom_blocks.js
@@ -19,6 +19,19 @@ Blockly.Blocks['create_task'] = {
       this.setHelpUrl("");
     }
   };
+Blockly.JavaScript['create_task'] = function(block) {
+  var taskName = Blockly.JavaScript.valueToCode(block, 'task_name', Blockly.JavaScript.ORDER_NONE) || "''";
+  var taskDescription = Blockly.JavaScript.valueToCode(block, 'task_description', Blockly.JavaScript.ORDER_NONE) || "''";
+  var dueDate = Blockly.JavaScript.valueToCode(block, 'due_date', Blockly.JavaScript.ORDER_NONE) || "null";
+  // Generate JavaScript code to create a task object
+  var code = `{
+    name: ${taskName},
+    description: ${taskDescription},
+    dueDate: ${dueDate}
+  }`;
+
+  return code;
+};
 
 Blockly.Blocks['text'] = {
   init: function() {
@@ -49,6 +62,18 @@ Blockly.Blocks['text'] = {
       this.setHelpUrl("");
     }
   };
+  Blockly.JavaScript['assign_task'] = function(block) {
+    var taskName = Blockly.JavaScript.valueToCode(block, 'task', Blockly.JavaScript.ORDER_NONE) || "''";
+    var user = Blockly.JavaScript.valueToCode(block, 'user', Blockly.JavaScript.ORDER_NONE) || "''";
+  
+    // Generate JavaScript code to create a task object
+    var code = `{
+      task: ${taskName},
+      user: ${user}
+    }`;
+  
+    return code;
+  };
   
   // Define the "set_dependency" block
   Blockly.Blocks['set_dependency'] = {
@@ -68,7 +93,19 @@ Blockly.Blocks['text'] = {
       this.setHelpUrl("");
     }
   };
+  Blockly.JavaScript['set_dependency'] = function(block) {
+    var dep_task = Blockly.JavaScript.valueToCode(block, 'dependent_task', Blockly.JavaScript.ORDER_NONE) || "''";
+    var dependency = Blockly.JavaScript.valueToCode(block, 'dependency_task', Blockly.JavaScript.ORDER_NONE) || "''";
   
+    // Generate JavaScript code to create a task object
+    var code = `{
+      dependent task: ${dep_task},
+      dependency task: ${dependency}
+    }`;
+  
+    return code;
+  };
+
   // Define the "update_task_status" block
   Blockly.Blocks['update_task_status'] = {
     init: function() {
@@ -87,7 +124,21 @@ Blockly.Blocks['text'] = {
       this.setHelpUrl("");
     }
   };
+  Blockly.JavaScript['update_task_status'] = function(block) {
+    var task_name = Blockly.JavaScript.valueToCode(block, 'task', Blockly.JavaScript.ORDER_NONE) || "''";
+    var status = Blockly.JavaScript.valueToCode(block, 'status', Blockly.JavaScript.ORDER_NONE) || "''";
   
+    // Generate JavaScript code to create a task object
+    var code = `{
+      task: ${task_name},
+      status: ${status}
+    }`;
+
+    //var displayCode = 'console.log(\'${code}\');';
+
+    return code;
+  };
+
   // Define the "filter_tasks" block
   Blockly.Blocks['filter_tasks'] = {
     init: function() {
@@ -117,4 +168,32 @@ Blockly.Blocks['text'] = {
       this.setHelpUrl("");
     }
   };
-  
+// Define the "date" block
+Blockly.Blocks['custom_date'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Date:");
+    this.appendValueInput("MONTH")
+        .setCheck("String")
+        .appendField("Month");
+    this.appendValueInput("DAY")
+        .setCheck("String")
+        .appendField("Day");
+    this.appendValueInput("YEAR")
+        .setCheck("String")
+        .appendField("Year");
+    this.setOutput(true, "Date");
+    this.setColour(230);
+    this.setTooltip("Create a custom date.");
+  }
+};
+Blockly.JavaScript['custom_date'] = function(block) {
+  var month = Blockly.JavaScript.valueToCode(block, 'MONTH', Blockly.JavaScript.ORDER_NONE) || '0';
+  var day = Blockly.JavaScript.valueToCode(block, 'DAY', Blockly.JavaScript.ORDER_NONE) || '0';
+  var year = Blockly.JavaScript.valueToCode(block, 'YEAR', Blockly.JavaScript.ORDER_NONE) || '0';
+
+  // Generate JavaScript code to create a Date object
+  var code = `new Date(${year}, ${month} - 1, ${day})`;
+
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};

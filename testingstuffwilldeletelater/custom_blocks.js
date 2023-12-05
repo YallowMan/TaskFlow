a144376f81535ec5cea99accd72e74a11442a977
Carlos Garcia <79238305+YallowMan@users.noreply.github.com>
2023-12-04 23:57:47 -0800

    Add files via upload

--- testingstuffwilldeletelater/custom_blocks.js
+++ testingstuffwilldeletelater/custom_blocks.js
@@ -1,150 +1,102 @@
-/* DEFINITIONS FOR BLOCKS */
-
-// Block definition to initialize DB. There is no inputs as this block will just generate the necessary code.
-Blockly.Blocks['initializeDB'] = {
-    init: function () {
-      this.appendDummyInput()
-        .appendField("Initializes DB & Classes");
-      this.setColour("#1D1D1D");
-      this.setTooltip("");
-      this.setHelpUrl("");
-    }
-  };
-  
-  Blockly.Blocks['restartDB'] = {
-    init: function () {
-      this.appendDummyInput()
-        .appendField("Restarts all DB");
-      this.setColour("#3CD0D5"); //Color for block is already set
-      this.setTooltip("");
-      this.setHelpUrl("");
-    }
-  };
-  
-  Blockly.Blocks['addTask'] = {
-    init: function() {
-      this.appendDummyInput()
-          .appendField("Add Task");
-      this.appendDummyInput()
-          .appendField("Name")
-          .appendField(new Blockly.FieldTextInput("Task Name"), "task_name");
-      this.appendDummyInput()
-          .appendField("Description")
-          .appendField(new Blockly.FieldTextInput("Task Description"), "task_description");
-      this.appendDummyInput()
-          .appendField("Due Date")
-          .appendField(new Blockly.FieldDate(), "due_date");
-      this.appendValueInput("assignTo")
-          .setCheck("identifier")
-          .appendField("Assign To");
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour("#FF0000"); // You can choose your desired color
-      this.setTooltip("Add a new task");
-      this.setHelpUrl("");
-    }
-  };
-  
-  Blockly.Blocks['updateTask'] = {
-    init: function() {
-      this.appendDummyInput()
-          .appendField("Update Task");
-      this.appendDummyInput()
-          .appendField("Task ID")
-          .appendField(new Blockly.FieldNumber(0, 1, Infinity, 1), "task_id");
-      this.appendDummyInput()
-          .appendField("New Name")
-          .appendField(new Blockly.FieldTextInput(""), "new_name");
-      this.appendDummyInput()
-          .appendField("New Description")
-          .appendField(new Blockly.FieldTextInput(""), "new_description");
-      this.appendDummyInput()
-          .appendField("New Due Date")
-          .appendField(new Blockly.FieldDate(), "new_due_date");
-      this.appendValueInput("new_assignTo")
-          .setCheck("identifier")
-          .appendField("New Assign To");
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour("#00FF00"); // You can choose your desired color
-      this.setTooltip("Update task details");
-      this.setHelpUrl("");
-    }
-  };
-  
-  
-  
-  /* GENERATORS FOR BLOCKS */
-  
-  // Generator block for initializing the database. This block should only be placed once at the beginning and can be removed later
-  python.pythonGenerator.forBlock['initializeDB'] = function(block, pythonGenerator) {
-    // Defines the variable code which stores the code that is outputted as a string
-    // Imports in sqlite & os packages to be used
-    var code = 'import sqlite3 \n';
-    code += 'import os \n\n';
-    // Makes a new database within the user's home directory
-    code += 'user_home_dir = os.path.expanduser("~")\n';
-    code += 'db_file_path = os.path.join(user_home_dir, "TaskManager.db")\n\n';
-    //Defines the connection & cursor used by sqlite
-    code += 'connection = sqlite3.connect(db_file_path)\n';
-    code += 'cursor = connection.cursor()\n';
-    // Add this code to the initializeDB block
-    code += 'cursor.execute(\'\'\'\n';
-    code += 'CREATE TABLE IF NOT EXISTS tasks(\n';
-    code += '    taskID INTEGER PRIMARY KEY,\n';
-    code += '    name VARCHAR(100),\n';
-    code += '    description TEXT,\n';
-    code += '    dueDate DATE,\n';
-    code += '    assignedTo VARCHAR(60)\n';
-    code += ');\n';
-  //Closes the SQL connection after commiting
-    code += 'connection.commit()\n';
-    code += 'connection.close()\n\n';
-  
-    return code;
-  };
-
-  python.pythonGenerator.forBlock['restartDB'] = function(block) {
-  // Follow the general formatting of initializeDB to write the code to delete every entry within the three tables ordersList, foodOrders, & drinkOrders
-  code += 'connection = sqlite3.connect(db_file_path)\n';
-  code += 'cursor = connection.cursor()\n';
-
-
-
-  code += 'connection.commit()\n';
-  code += 'connection.close()\n\n';
-  return [code]
-}
-
-  Blockly.Python['addTask'] = function(block) {
-    var taskName = block.getFieldValue('task_name');
-    var taskDescription = block.getFieldValue('task_description');
-    var dueDate = block.getFieldValue('due_date');
-    var assignedTo = Blockly.Python.valueToCode(block, 'assignTo', Blockly.Python.ORDER_ATOMIC);
-  
-    var code = 'connection = sqlite3.connect(db_file_path)\n';
-    code += 'cursor = connection.cursor()\n\n';
-    code += 'cursor.execute("INSERT INTO tasks (name, description, dueDate, assignedTo) VALUES (?, ?, ?, ?)", ("' + taskName + '", "' + taskDescription + '", "' + dueDate + '", "' + assignedTo + '"))\n';
-    code += 'connection.commit()\n';
-    code += 'connection.close()\n\n';
-  
-    return code;
-  };
-
-  
-  Blockly.Python['updateTask'] = function (block) {
-    var taskID = block.getFieldValue('task_id');
-    var newName = block.getFieldValue('new_name');
-    var newDescription = block.getFieldValue('new_description');
-    var newDueDate = block.getFieldValue('new_due_date');
-    var newAssignTo = Blockly.Python.valueToCode(block, 'new_assignTo', Blockly.Python.ORDER_ATOMIC);
-
-    var code = 'connection = sqlite3.connect(db_file_path)\n';
-    code += 'cursor = connection.cursor()\n\n';
-    code += 'cursor.execute("UPDATE tasks SET name=?, description=?, dueDate=?, assignedTo=? WHERE taskID=?", ("' + newName + '", "' + newDescription + '", "' + newDueDate + '", "' + newAssignTo + '", ' + taskID + '))\n';
-    code += 'connection.commit()\n';
-    code += 'connection.close()\n\n';
-
-    return code;
+Blockly.Blocks['initialize_database'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Initialize Database");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip("Initialize the SQLite database");
+    this.setHelpUrl("");
+  }
 };
-  
+
+Blockly.Blocks['create_task'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Create Task");
+    this.appendValueInput("task_name")
+        .setCheck("String")
+        .appendField("Name");
+    this.appendValueInput("task_description")
+        .setCheck("String")
+        .appendField("Description");
+    this.appendValueInput("due_date")
+        .setCheck("String")
+        .appendField("Due Date");
+    this.appendValueInput("assigned_person")
+        .setCheck("String")
+        .appendField("Assigned Person");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+    this.setTooltip("Create a new task in the database");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['update_task'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Update Task");
+    this.appendValueInput("task_id")
+        .setCheck("Number")
+        .appendField("Task ID");
+    this.appendValueInput("new_description")
+        .setCheck("String")
+        .appendField("New Description");
+    this.appendValueInput("new_due_date")
+        .setCheck("String")
+        .appendField("New Due Date");
+    this.appendValueInput("new_assigned_person")
+        .setCheck("String")
+        .appendField("New Assigned Person");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(270);
+    this.setTooltip("Update task information in the database");
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['initialize_database'] = function(block) {
+  // Generate JavaScript code to initialize the SQLite database
+  var code = `
+    // Code to initialize the SQLite database
+    // Example: var db = openDatabase('mydb', '1.0', 'My first database', 2 * 1024 * 1024);
+  `;
+  return code;
+};
+
+Blockly.JavaScript['create_task'] = function(block) {
+  var taskName = Blockly.JavaScript.valueToCode(block, 'task_name', Blockly.JavaScript.ORDER_ATOMIC);
+  var taskDescription = Blockly.JavaScript.valueToCode(block, 'task_description', Blockly.JavaScript.ORDER_ATOMIC);
+  var dueDate = Blockly.JavaScript.valueToCode(block, 'due_date', Blockly.JavaScript.ORDER_ATOMIC);
+  var assignedPerson = Blockly.JavaScript.valueToCode(block, 'assigned_person', Blockly.JavaScript.ORDER_ATOMIC);
+
+  // Generate JavaScript code to create a task in the SQLite database
+  var code = `
+    // Code to create a task in the SQLite database
+    // Example: db.transaction(function(tx) {
+    //   tx.executeSql('INSERT INTO tasks (name, description, due_date, assigned_person) VALUES (${taskName}, ${taskDescription}, ${dueDate}, ${assignedPerson})');
+    // });
+  `;
+  return code;
+};
+
+Blockly.JavaScript['update_task'] = function(block) {
+  var taskId = Blockly.JavaScript.valueToCode(block, 'task_id', Blockly.JavaScript.ORDER_ATOMIC);
+  var newDescription = Blockly.JavaScript.valueToCode(block, 'new_description', Blockly.JavaScript.ORDER_ATOMIC);
+  var newDueDate = Blockly.JavaScript.valueToCode(block, 'new_due_date', Blockly.JavaScript.ORDER_ATOMIC);
+  var newAssignedPerson = Blockly.JavaScript.valueToCode(block, 'new_assigned_person', Blockly.JavaScript.ORDER_ATOMIC);
+
+  // Generate JavaScript code to update task information in the SQLite database
+  var code = `
+    // Code to update task information in the SQLite database
+    // Example: db.transaction(function(tx) {
+    //   tx.executeSql('UPDATE tasks SET description = ${newDescription}, due_date = ${newDueDate}, assigned_person = ${newAssignedPerson} WHERE id = ${taskId}');
+    // });
+  `;
+  return code;
+};
